ca4d0c4d3c4dbe2cdd10702c41b1ee7a14755d9f
Seshanth-Sivan <seshanthsivan@karunya.edu.in>
2025-04-29 10:19:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const display = document.getElementById('display');
+
+function appendNumber(number) {
+    display.value += number;
+}
+
+function appendOperator(operator) {
+    display.value += operator;
+}
+
+function appendDot() {
+    if (!display.value.includes('.')) {
+        display.value += '.';
+    }
+}
+
+function clearDisplay() {
+    display.value = '';
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch {
+        display.value = 'Error';
+    }
+}
